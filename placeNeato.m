901d0c88bf27029bf0cd764bef848f57cce60825
lilibaker <65792993+lilibaker@users.noreply.github.com>
2022-04-29 13:04:49 -0400

    Add placeNeato and collectScans for gettign LIDAR Data

---
+++ placeNeato.m
@@ -0,0 +1,24 @@
+% For simulated Neatos only:
+% Place the Neato in the specified x, y position and specified heading vector.
+function placeNeato(posX, posY, headingX, headingY, posZ)
+    if nargin < 5
+        posZ = 1.0;
+    end
+    svc = rossvcclient('gazebo/set_model_state');
+    msg = rosmessage(svc);
+
+    msg.ModelState.ModelName = 'neato_standalone';
+    startYaw = atan2(headingY, headingX);
+    quat = eul2quat([startYaw 0 0]);
+
+    msg.ModelState.Pose.Position.X = posX;
+    msg.ModelState.Pose.Position.Y = posY;
+    msg.ModelState.Pose.Position.Z = posZ;
+    msg.ModelState.Pose.Orientation.W = quat(1);
+    msg.ModelState.Pose.Orientation.X = quat(2);
+    msg.ModelState.Pose.Orientation.Y = quat(3);
+    msg.ModelState.Pose.Orientation.Z = quat(4);
+
+    % put the robot in the appropriate place
+    ret = call(svc, msg);
+end
